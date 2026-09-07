6725796c1d4906ceb5168a466b42bc13c2f91d40
panchal401640-ux <panchal401640@gmail.com>
2026-09-07 12:40:40 +0530

    fix: create layout/MobileCTA re-export (MobileStickyBar, FloatingWhatsAppButton, CTASection)

---
+++ src/components/layout/MobileCTA.tsx
@@ -0,0 +1,9 @@
+import MobileStickyBarComponent from '@/components/MobileStickyBar';
+import WhatsAppButtonComponent from '@/components/WhatsAppButton';
+import CTASectionComponent from '@/components/CTASection';
+
+export const MobileStickyBar = MobileStickyBarComponent;
+export const FloatingWhatsAppButton = WhatsAppButtonComponent;
+export const CTASection = CTASectionComponent;
+
+export default MobileStickyBarComponent;
