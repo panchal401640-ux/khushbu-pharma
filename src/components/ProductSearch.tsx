f7ff125a89d718c8faf4085885d64a389cf85577
panchal401640-ux <panchal401640@gmail.com>
2026-09-08 12:37:37 +0530

    Fix: add missing quotes in className

--- src/components/ProductSearch.tsx
+++ src/components/ProductSearch.tsx
@@ -72,7 +72,7 @@ export default function ProductSearch({
   };
 
   return (
-    <div ref={containerRef} className={`relative ${className}`}>
+    <div ref={containerRef} className="`relative ${className"`}>
       <div className="relative">
         <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-400" />
         <input
